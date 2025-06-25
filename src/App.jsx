f02708677352077e83e6e1070812cfe940293c73
khushi santosh patil <khushipatil1523@gmail.com>
2025-06-25 15:23:22 +0530

    implement_animation_for_text_in_hero_section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
   return (
-    <>
-      <div className="flex-center h-[100vh]">
-        <h1 className="text-3xl text-indigo-300">HELLO GSAP</h1>
-      </div>
-    </>
+    <main>
+        <Navbar/>
+        <Hero/>
+    </main>
   );
 }
